7c44c62acbc1c95194cf287ed54ad84243c726bc
Rmjp <rmjp15.rmt@gmail.com>
2020-12-17 16:58:06 +0700

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,27 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+
+#input and binary
 img = cv.imread('img1.jpg',-1)
 gr = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-ret,bi = cv.threshold(gr, 145, 255, cv.THRESH_BINARY_INV)
+ret,bi = cv.threshold(gr, 150, 255, cv.THRESH_BINARY_INV)
+
+#preprocessing(Gaussian, ero)
 blur = cv.GaussianBlur(bi,(3, 3),0)
-cv.imshow("blur", blur)
-cv.imshow("gr", gr)
+
+kernel = np.ones((5,5),np.uint8)
+opening = cv.morphologyEx(blur, cv.MORPH_OPEN, kernel)
+
+kernel = np.ones((3,3),np.uint8)
+closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
+
+ret,pre = cv.threshold(closing, 160, 255, cv.THRESH_BINARY)
+
+#imshow
+cv.imshow("clo", closing)
+cv.imshow("ope", opening)
+cv.imshow("bi", bi)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
