c12f30b7c08432d41f675e2778f6045b9f3e7570
Rmjp <rmjp15.rmt@gmail.com>
2021-01-19 19:50:41 +0700

    Update test_cam.py

--- test_cam.py
+++ test_cam.py
@@ -10,6 +10,10 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 # allow the camera to warmup
 time.sleep(0.1)
+
+size = camera.resolution
+result = cv2.VideoWriter("picam.avi", cv2.VideoWriter_fourcc(*'MJPG'), camera.framerate, size)
+
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
@@ -17,6 +21,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	image = frame.array
 	# show the frame
 	cv2.imshow("Frame", image)
+
+    result.write(image)
+
 	key = cv2.waitKey(1) & 0xFF
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
